aa432a3f0abe867919800da96c51cfab31fde0e3
Ecohnoch <542305306@qq.com>
2021-02-01 15:09:37 +0800

    1.0.6

--- flask_autoapi/__init__.py
+++ flask_autoapi/__init__.py
@@ -9,6 +9,10 @@ def register(config):
     service_object = RestfulServiceEnroll(config=config)
     return service_object.app, service_object.api
 
+def register_with_app(config, app, api):
+    service_object = RestfulServiceEnroll(config=config, app=app, api=api)
+    return service_object.app, service_object.api
+
 def launch(config, host=None):
     service_object = RestfulServiceEnroll(config=config)
     service_object.launch_app(host)
